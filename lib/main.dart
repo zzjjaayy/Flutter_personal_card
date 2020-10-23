81d3c095f730fef973319fb0deae96a06b663504
zzjjaayy <jayprimerathod@gmail.com>
2020-10-23 22:44:41 +0530

    Hot Reload and Hot Restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+// One of the most powerful features of flutter is "Hot Reload"
+// To use this features, you need to use Stateless and Stateful widgets
+// {will be explained later}
+// doesn't have to build the whole code again but to just load the
+// parent build function where the change is made.
+// this many times doesn't even result in data reset which is obvious in rebuild.
+
+// Flutter "Hot Restart" resets the state of the app i.e the data reset
+// but it still takes way less time than rebuilding
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
