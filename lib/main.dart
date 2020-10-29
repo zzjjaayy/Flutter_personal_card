c184182ee026be02c842d2e30deba11f5f52a2d6
zzjjaayy <jayprimerathod@gmail.com>
2020-10-29 22:42:17 +0530

    Using Card and Padding Widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,9 @@ void main() {
   runApp(MyApp());
 }
 
-// Using icons in flutter app :
-// Instead of using image resources for icons, a "Icon" widget is used.
-// It adds flexibility and it is not displayed but drawn.
-// to get different icons go to -> https://www.materialpalette.com/icons
+// Using padding in Widgets which don't support it
+// The unsupported widget can be enclosed in a "Padding" widget
+// The padding widget does not actually work like padding but like margin.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -40,53 +39,54 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'SourceSansPro',
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone, // the "Icons" class supports Material icons
-                      // size: 100.0, // for changing size of the icon
-                      color: Colors.teal.shade500,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "+91 112 2334 455",
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal[900],
-                          // the [] brackets are used for defining shade {the higher -> the darker}
-                          // It can also be done as "teal.shade900"
-                          fontFamily: 'SourceSansPro'),
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal.shade500,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "+91 112 2334 455",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal[900],
+                            fontFamily: 'SourceSansPro'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal.shade500,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "example@gmail.com",
-                      style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal[900],
-                          fontFamily: 'SourceSansPro'),
-                    ),
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal.shade500,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "example@gmail.com",
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.teal[900],
+                            fontFamily: 'SourceSansPro'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ],
